20afc3806dd1ea60b7f3657d78259c1e34d3efeb
Gustavo Reis Silva <49646779+gustavoreis20@users.noreply.github.com>
2020-10-08 11:24:44 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,63 @@
+#include <Arduino.h>
+
+
+// --- Mapeamento de Hardware ---
+//TODOS ESSES PINOS SÃO TEMPORÁRIOS
+// SERÁ NECESSÁRIO MUDAR.
+#define trig 9  // definir o Pino do Arduino que será a saída de trigger
+#define echo 8  // definir o Pino 8 do Arduino que será a entrada de echo
+
+
+void trigPuls();  
+void sonarBegin();
+
+
+float pulse;     
+float dist_cm;   
+
+
+void setup() 
+{
+    pinMode(trig, OUTPUT);   
+    pinMode(echo, INPUT);    
+
+    digitalWrite(trig, LOW);
+    Serial.begin(9600);      
+
+}
+
+
+void loop() 
+{
+    sonarBegin();
+}
+
+
+// IMPLEMENTAÇÃO DAS FUNÇÕES
+void trigPulse()
+{
+    digitalWrite(trig, HIGH);  
+    delayMicroseconds(10);     
+    digitalWrite(trig, LOW);   
+}
+
+
+void sonarBegin()
+{
+    trigPulse();                 
+    pulse = pulseIn(echo, HIGH, 200000); 
+    dist_cm = pulse/58.82;      
+
+    Serial.println(dist_cm);     
+    delay(200);
+}
+
+
+
+
+
+
+
+
+
+
