bcf927e495503633484cf06f02df656875f7591a
beyondbirthday737 <49646779+beyondbirthday737@users.noreply.github.com>
2020-11-17 14:09:04 -0300

    Update t900.2.0.ino

--- t900.2.0.ino
+++ t900.2.0.ino
@@ -1,4 +1,6 @@
-#include <Ultrasonic.h>           
+#include <Ultrasonic.h> 
+
+// Configuração dos sensores
 #define TRIGGER_PIN1 4               
 #define ECHO_PIN1    5 
 
@@ -8,7 +10,7 @@
 #define TRIGGER_PIN3 14              
 #define ECHO_PIN3    15
               
-
+//configuração dos motores
 #define IN1 6                       
 #define IN2 7                       
 #define IN3 8                       
@@ -17,11 +19,15 @@
 #define ENA 10                      
 #define ENB 11
 
+// configuração do LED
+#define LED 16
+
 
 Ultrasonic ultrasonic_frente(TRIGGER_PIN1, ECHO_PIN1);
 Ultrasonic ultrasonic_esquerda(TRIGGER_PIN2, ECHO_PIN2);
 Ultrasonic ultrasonic_direita(TRIGGER_PIN3, ECHO_PIN3);
 
+unsigned long int tempoAnterior = 0;
 
 float distancia_frente();
 float distancia_direita();                 
@@ -35,6 +41,7 @@ void robo_parado();
 void decisao_esquerda();
 void decisao_direita();
 
+void ligar_uvc();
 
 void setup() 
 {
@@ -45,8 +52,9 @@ void setup()
   pinMode(ENA,OUTPUT);              
   pinMode(ENB,OUTPUT);              
   analogWrite(ENA,120);             
-  analogWrite(ENB,120);             
-  delay(1000);                    
+  analogWrite(ENB,120); 
+  pinMode(16, OUTPUT); // led
+  delay(1000); 
 } //end setup
 
 
@@ -54,19 +62,28 @@ void loop()
 {
   while(1)
   {
-    robo_frente();                    
-    float dist_cm_frente = distancia_frente();
-    float dist_cm_direita = distancia_direita();    
-    float dist_cm_esquerda = distancia_esquerda();         
-  
-    if(dist_cm_frente <= 70) // caso a distancia medida na frente for menor que 70cm                
+    if(millis() - tempoAnterior >= 3000)
     {
-       if(dist_cm_direita > dist_cm_esquerda) { decisao_direita(); } // caso a distancia medida na direita for maior que a esquerda o robo irá para a direita
-       if(dist_cm_direita < dist_cm_esquerda) { decisao_esquerda();} // caso a distancia medida na direita for menor que a esquerda o robo irá para a a esquerda
-       if(dist_cm_direita == dist_cm_esquerda){ decisao_esquerda();} // caso as duas medidas forem iguais o robo irá para a esquerda. ESSA DECISÃO É SÓ PARA EVITAR QUE O ROBO FIQUE CONFUSO
-    } 
-
-    delay(100); 
+       tempoAnterior = millis();
+       robo_parado();
+       ligar_uvc();
+    }
+    
+    else
+    {
+      robo_frente();                    
+      float dist_cm_frente = distancia_frente();
+      float dist_cm_direita = distancia_direita();    
+      float dist_cm_esquerda = distancia_esquerda();         
+    
+      if(dist_cm_frente <= 70) // caso a distancia medida na frente for menor que 70cm                
+      {
+         if(dist_cm_direita > dist_cm_esquerda) { decisao_direita(); } // caso a distancia medida na direita for maior que a esquerda o robo irá para a direita
+         if(dist_cm_direita < dist_cm_esquerda) { decisao_esquerda();} // caso a distancia medida na direita for menor que a esquerda o robo irá para a a esquerda
+         if(dist_cm_direita == dist_cm_esquerda){ decisao_esquerda();} // caso as duas medidas forem iguais o robo irá para a esquerda. ESSA DECISÃO É SÓ PARA EVITAR QUE O ROBO FIQUE CONFUSO
+      } 
+    }
+    delay(100);
   }                          
 } 
 
@@ -152,3 +169,11 @@ void decisao_direita()
   robo_parado();
   delay(500);
 }
+
+void ligar_uvc()
+{
+  digitalWrite(16, HIGH);
+  delay(3000);
+  digitalWrite(16, LOW);  
+}
+
